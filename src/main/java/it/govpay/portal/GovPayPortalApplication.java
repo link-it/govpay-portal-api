b3cf79989040836e977db57b934965f86c138503
Giuliano Pintori <pintori@link.it>
2026-03-12 17:29:30 +0100

    Rilascio versione 1.1.2

--- src/main/java/it/govpay/portal/GovPayPortalApplication.java
+++ src/main/java/it/govpay/portal/GovPayPortalApplication.java
@@ -4,10 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class GovPayPortalApplication {
+public class GovPayPortalApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GovPayPortalApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GovPayPortalApplication.class, args);
